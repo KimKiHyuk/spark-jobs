7d63571a3d818317a7c67afde563987a96da542d
key kim <keyhyuk.kim@gmail.com>
2020-11-21 15:50:37 +0000

    Add dotnet for emr

---
+++ dotnet/emrApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace emrApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("word_count_sample")
+                .GetOrCreate();
+
+        }
+    }
+}
