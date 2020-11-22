3830001b3051b57f449409688295a5dc97a2a722
key kim <keyhyuk.kim@gmail.com>
2020-11-22 17:46:02 +0000

    dotnet spark program done

--- dotnet/emrApp/Program.cs
+++ dotnet/emrApp/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Types;
+using System.Collections.Generic;
 
 namespace emrApp
 {
     class Program
     {
+        private class Address 
+        {
+            public Address(string[] raw) 
+            {
+                city = raw[0];
+                state = raw[1];
+                postal = raw[2];
+            }
+            string city;
+            string state;
+            string postal;
+
+            public object[] toArray()
+            {
+                return new object[] {city, state, postal};
+            }
+        }
         static void Main(string[] args)
         {
             SparkSession spark = SparkSession
@@ -16,13 +35,32 @@ namespace emrApp
             DataFrame dataFrame = spark
                 .Read()
                 .Format("avro")
-                .Load("s3a://spark-data-vjal1251/topics/orders/partition=0");
-            
+                .Load(args[0]);
+
             var rows = dataFrame.Select("address").Collect();
 
+            var seq = new List<GenericRow>();
+
             foreach (var row in rows) {
-                Console.WriteLine(row);
+                var str = row.ToString();
+                var address = new Address(str.Substring(1, str.Length - 2).Split(','));
+                seq.Add(new GenericRow(address.toArray()));
             }
+
+            var schema = new StructType(
+                new List<StructField>() {
+                    new StructField("city", new StringType()),
+                    new StructField("state", new StringType()),
+                    new StructField("postal", new StringType())
+                }
+            );
+
+            var addressDataFrame = spark.CreateDataFrame(seq, schema);
+            addressDataFrame.Show();
+
+            addressDataFrame.GroupBy("city").Count();
+            addressDataFrame.Show();
+            addressDataFrame.Write().Format("json").Save($"{args[1]}/{DateTime.UtcNow.ToString().Replace(" ", "-")}");
         }
     }
 }
