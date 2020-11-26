6c057c51ffcad93786d6bab29c97279d17cc1eb4
key kim <keyhyuk.kim@gmail.com>
2020-11-26 07:49:38 +0000

    Update emr app

--- dotnet/emrApp/Program.cs
+++ dotnet/emrApp/Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Spark.Sql;
 using Microsoft.Spark.Sql.Types;
-using System.Collections.Generic;
+using static Microsoft.Spark.Sql.Functions;
 
 namespace emrApp
 {
+    interface IDataFrame {
+        StructType GetSchema();
+    }
+
     class Program
     {
-        private class Address 
+        private class Region: IDataFrame
         {
-            public Address(string[] raw) 
+            
+            int regionCode;
+            
+            public StructType GetSchema()
             {
-                city = raw[0];
-                state = raw[1];
-                postal = raw[2];
-            }
-            string city;
-            string state;
-            string postal;
-
-            public object[] toArray()
-            {
-                return new object[] {city, state, postal};
+                return new StructType(
+                    new List<StructField>() {
+                        new StructField("city", new StringType()),
+                        new StructField("count", new IntegerType()),
+                    }
+                );
             }
         }
         static void Main(string[] args)
@@ -37,30 +40,16 @@ namespace emrApp
                 .Format("avro")
                 .Load(args[0]);
 
-            var rows = dataFrame.Select("address").Collect();
+            var s = dataFrame["itemid"];
+        
+            Func<Column, Column> udf = Udf<string, string[]>(
+                str => str.Split('_')
+            );  // city_23 --> 23 --> 2
 
-            var seq = new List<GenericRow>();
+            dataFrame = dataFrame.WithColumn("Region", udf(dataFrame["itemid"]));
+            dataFrame.Show();
 
-            foreach (var row in rows) {
-                var str = row.ToString();
-                var address = new Address(str.Substring(1, str.Length - 2).Split(','));
-                seq.Add(new GenericRow(address.toArray()));
-            }
-
-            var schema = new StructType(
-                new List<StructField>() {
-                    new StructField("city", new StringType()),
-                    new StructField("state", new StringType()),
-                    new StructField("postal", new StringType())
-                }
-            );
-
-            var addressDataFrame = spark.CreateDataFrame(seq, schema);
-            addressDataFrame.Show();
-
-            addressDataFrame.GroupBy("city").Count();
-            addressDataFrame.Show();
-            addressDataFrame.Write().Format("json").Save($"{args[1]}/{DateTime.UtcNow.ToString().Replace(" ", "-")}");
+            //spark.Stop();
         }
     }
 }
